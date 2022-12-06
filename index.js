28dc9a0a8499f8f3fcfd17602e8fbda6c47ea3b7
michelles11 <113190099+michelles11@users.noreply.github.com>
2022-12-06 10:15:06 -0500

    aertewt

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ change themes as desired
 -Use both map and filter to create new arrays to iterate through
 -Create two separate themes using CSS Custom Properties and allow
 users to change theme */
+
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+<h1>Hello Vite!!!!!!!</h1>
+<a href = "https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>`;
