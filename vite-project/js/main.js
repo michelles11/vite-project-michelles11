319cbc85b70205a4e0288d0973b946024882ed45
michelles11 <113190099+michelles11@users.noreply.github.com>
2022-12-07 10:15:19 -0500

    12/7/22

---
+++ vite-project/js/main.js
@@ -0,0 +1,8 @@
+import "../styles/style.css";
+
+document.querySelector("#app").innerHTML = `
+<h1>Hello????</h1>
+  <div>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>`;
