683279d4b02fd2966a5f4a191356f5f5e66340af
michelles11 <113190099+michelles11@users.noreply.github.com>
2022-12-15 09:43:40 -0500

    Update main.js

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,8 +1,10 @@
 import "../styles/style.css";
+import { menu } from "./menu";
 
 document.querySelector("#app").innerHTML = `
 <h1>Hello????</h1>
   <div>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>`;
+console.log(menu);
