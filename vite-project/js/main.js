fcf16b27581a1502ac4941c76521c659c5c3e183
michelles11 <113190099+michelles11@users.noreply.github.com>
2022-12-16 10:12:59 -0500

    12.16.22

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,9 @@
 import "../styles/style.css";
 import { menu } from "./menu";
-
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 document.querySelector("#app").innerHTML = `
 <h1>Hello????</h1>
   <div>
