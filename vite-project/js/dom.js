a74e814c22a488072e50d85963aab17ace23cb79
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-01-12 10:16:31 -0500

    1.12.23

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,3 +1,8 @@
 const DOMSelectors = {
-  document.getElementById()
+  red: document.getElementById("red"),
+  blue: document.getElementById("blue"),
+  green: document.getElementById("green"),
+  orange: document.getElementById("orange"),
+  Display: document.getElementById("Display"),
 };
+export { DOMSelectors };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,5 +1,6 @@
 import "../styles/style.css";
 import { list } from "./list";
+import { DOMSelectors } from "./dom";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
@@ -7,15 +8,55 @@ AOS.init();
 
 console.log(list);
 
-document.querySelector("#btn").addEventListener("click"), DOM;
-function makeCards() {
-  list.forEach((school) => {
-    DOMSelectors.Display.insertAdjacentHTML(
-      "beforeend"`<div class="card">
+DOMSelectors.red.addEventListener("click", function () {
+  DOMSelectors.Display.innerHTML = "";
+  list
+    .filter((school) => school.color.includes("red"))
+    .forEach((school) =>
+      DOMSelectors.Display.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card">
             <h2>${school.name}</h2>
             <img class="image" src="${school.image}" alt=""></div>`
+      )
     );
-  });
-}
-
-makeCards();
+});
+DOMSelectors.blue.addEventListener("click", function () {
+  DOMSelectors.Display.innerHTML = "";
+  list
+    .filter((school) => school.color.includes("blue"))
+    .forEach((school) =>
+      DOMSelectors.Display.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card">
+            <h2>${school.name}</h2>
+            <img class="image" src="${school.image}" alt=""></div>`
+      )
+    );
+});
+DOMSelectors.green.addEventListener("click", function () {
+  DOMSelectors.Display.innerHTML = "";
+  list
+    .filter((school) => school.color.includes("green"))
+    .forEach((school) =>
+      DOMSelectors.Display.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card">
+            <h2>${school.name}</h2>
+            <img class="image" src="${school.image}" alt=""></div>`
+      )
+    );
+});
+DOMSelectors.orange.addEventListener("click", function () {
+  DOMSelectors.Display.innerHTML = "";
+  list
+    .filter((school) => school.color.includes("orange"))
+    .forEach((school) =>
+      DOMSelectors.Display.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card">
+            <h2>${school.name}</h2>
+            <img class="image" src="${school.image}" alt=""></div>`
+      )
+    );
+});
